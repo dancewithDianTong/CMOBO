3d05a3998d06ee93684485baeea76478faec11a4
dancewithDianTong <118501246+dancewithDianTong@users.noreply.github.com>
2024-07-28 19:50:21 -0500

    modified params

--- peni.py
+++ peni.py
@@ -1,14 +1,14 @@
-import math
-def peni(V, X, T, S, F, S_f, H):
+import numpy as np
+def peni(variable):
     '''
     input:
-    V: culture medium volume L
-    X: biomass concentration g/L 
-    T: temperature K 
-    S: glucose substrate concentration g/L 
-    F: substrate feed rate L/hr 
-    S_f: substrate feed concentration g /L 
-    H: H+ concentration mol/L 
+    V: culture medium volume L [60, 20]
+    X: biomass concentration g/L [0.05, 18]
+    T: temperature K [293, 303]
+    S: glucose substrate concentration g/L [0.05, 18]
+    F: substrate feed rate L/hr [0.01, 0.50]
+    S_f: substrate feed concentration g /L [500, 700]
+    pH: pH [5, 6.5]
 
     return:
     P: concentration of penicillin (>=0)
@@ -16,6 +16,7 @@ def peni(V, X, T, S, F, S_f, H):
     CO2: CO2 concentration (>=0)
     '''
     #initialize
+    V, X, T, S, F, S_f, pH = variable[0], variable[1], variable[2], variable[3], variable[4], variable[5], variable[6]
     P = 0
     t= 0
     CO2 = 0
@@ -23,6 +24,7 @@ def peni(V, X, T, S, F, S_f, H):
     process_end = False
     iter = 0
 
+    H = 10**(-pH)
     lamb = 2.5* 10**(-4)
     T0 = 273.1
     Tv = 373.1
@@ -36,7 +38,7 @@ def peni(V, X, T, S, F, S_f, H):
     Kx = 0.15
     kg = 7*10**3
     Eg = 5100 
-    R = 8.314 #gas constant
+    R = R = 1.9872
     kd = 10**33
     Ed = 50000
     mup = 0.005
@@ -50,10 +52,10 @@ def peni(V, X, T, S, F, S_f, H):
     while not process_end:
         iter +=1
         #dV
-        dV = F - V*lamb*(math.exp(5* (T- T0)/(Tv-T0))-1)
+        dV = F - V*lamb*(np.exp(5* ((T- T0)/(Tv-T0)))-1)
 
         #dX
-        mu = (mux/(1+K1/H+H/K2))*(S/(Kx*X+S))*(kg*math.exp(-Eg/(R*T))-kd*math.exp(-Ed/(R*T)))
+        mu = (mux/(1+K1/H+H/K2))*(S/(Kx*X+S))*(kg*np.exp(-Eg/(R*T))-kd*np.exp(-Ed/(R*T)))
         dX = mu* X - (X/V)*dV
 
         #dP
@@ -77,8 +79,9 @@ def peni(V, X, T, S, F, S_f, H):
         #whether to stop process
         if V > 180: #exceed maximum volume
             process_end = True
-        elif dP < 0.0000001: # P converges
+        elif dP < 10**(-12): # P converges
             process_end = True
         elif iter > 1000: #max iteration
             process_end = True
     return P, t, CO2
+
