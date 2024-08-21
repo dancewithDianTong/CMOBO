129c49e67a47a80953c391412e0fc3be846c90d3
Diantong Li <dancewithdiantong@gmail.com>
2024-08-21 12:51:11 -0500

    Add files via upload

--- metrics.py
+++ metrics.py
@@ -22,3 +22,33 @@ def HV(Y, ref):
 def violation(Y, ref):
     vio_raw = - torch.min(Y -ref, torch.zeros_like(Y))
     return (vio_raw).sum(-1)
+
+###Cumulative regret##
+
+def cum_regret(tensor, ub = 10000):
+    # Initialize the result tensor with the original tensor
+    result = tensor.clone()
+    result = ub - result
+    
+    # Perform cumulative sum along each dimension
+    for dim in range(tensor.dim()):
+        result = result.cumsum(dim)
+    return result
+###Cumulative violation###
+def cum_violation(tensor, ref):
+    result = tensor.clone()
+    vio = violation(result, ref)
+    for dim in range(tensor.dim()):
+        vio = vio.cumsum(dim)
+    return vio
+###constraint regret###
+def constraint_regret(Y, ref, hv):
+    vio = violation(Y, ref)
+    hv = 10000 - hv
+    hv = (hv - hv.min()) / (hv.max() - hv.min())
+    vio = (vio - vio.min()) / (vio.max() - vio.min())
+    Sum = hv + vio
+    for dim in range(Sum.dim()):
+        Sum = torch.minimum(Sum, Sum.cummin(dim=dim).values)
+    return Sum
+
